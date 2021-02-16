bf57424c3bfdfc05cee425528de34b1342e4dc3d
Behrud411 <77116718+Behrud411@users.noreply.github.com>
2021-02-17 05:59:39 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,189 @@
+#! /system/bin/sh
+res="\033[7m"
+hitam="\033[0;30m"
+abu2="\033[1;30m"
+putih="\033[0;37m"
+putih2="\033[1;37m"
+red="\033[0;31m"
+red2="\033[1;31m"
+green="\033[0;32m"
+green2="\033[1;32m"
+yellow="\033[0;33m"
+yellow2="\033[1;33m"
+blue="\033[0;34m"
+blue2="\033[1;34m"
+purple2="\033[1;35m"
+lblue="\033[0;36m"
+lblue2="\033[1;36m"
+
+
+echo ""
+echo -ne "${green2}^         "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "▀  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▀"
+sleep 0.05
+echo -ne "▒           ${green2}^\n"
+sleep 0.05
+echo -ne "${green2}^           "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "█             ${green2}^\n"
+sleep 0.05
+echo -ne "${green2}^         ${putih2}  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "█             ${green2}^\n"
+sleep 0.05
+echo -ne "${green2}^         ${putih2}  "
+sleep 0.05
+echo -ne "▒█    "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█    "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█     "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█ "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "█  "
+sleep 0.05
+echo -ne "▒"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "█"
+sleep 0.05
+echo -ne "▄"
+sleep 0.05
+echo -ne "▒           ${green2}^\n\n"
+
+for i in {0..58}
+do
+   echo -ne "${blue2}▄"
+sleep 0.05
+done
+
+echo ""
+
+
+
+
